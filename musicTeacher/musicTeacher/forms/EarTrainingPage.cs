81f2abd185028b487645e74ead2fd8bb6163ad91
sgramajo <stacygramajo03@gmail.com>
2014-11-04 15:53:16 -0500

    Created the screen for EarTraining Mode

---
+++ musicTeacher/musicTeacher/forms/EarTrainingPage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace musicTeacher.forms
+{
+    public partial class EarTrainingPage : Form
+    {
+        public EarTrainingPage()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EarTrainingPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
